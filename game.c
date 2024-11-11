9438bfb22a23ed6630280244614c4ae23a583de8
anorisno <shukin.viktor22@gmail.com>
2024-11-11 16:39:11 +0300

    fix: bug with memory

--- game.c
+++ game.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 
-void prepareLevel(int s, char lvl[s][s]);
+void prepareLevel(int s, char lvl[]);
+
+char room[10*10];
 
 int main(){
-	char room[10][10];
-
+	// char room[10*10];
+	printf("before prepare\n");
 	prepareLevel(10, room);
+	printf("after prepare\n");
 	for(int i=0; i<10; i++){
-		for(int j=0; j<10; i++){
-			printf("%c", room[i][j]);
+		for(int j=0; j<10; j++){
+			printf("%c", room[i * 10 + j]);
 	       }
-       }	       
+       } 
        return 0;
 }
 
-void prepareLevel(int size, char level[size][size]){
-	for(int i=0; i<10; i++){
-		for(int j=0; j<10; j++){
-			level[i][j] = '-';
+void prepareLevel(int size, char level[]){
+	printf("2\n");
+	for(int i=0; i<size; i++){
+		for(int j=0; j<size; j++){
+			level[i*size + j] = '-';
 		}	
 	}
+	printf("3\n");
 }
